51e157774a00ec94f7517d2fb21e4e283b33cbc6
JayanthiTG <112747929+JayanthiTG@users.noreply.github.com>
2022-09-05 19:56:08 +0530

    Add files via upload
    Linking Two Packages(by creating object) and Taking input(name) from user

---
+++ CCBO.java
@@ -0,0 +1,11 @@
+package jayanthi;
+import java.util.*;
+public class CCBO {
+public void display() {
+	System.out.println("Enter your name :");
+	Scanner sc = new Scanner(System.in);
+	String name = sc.nextLine();
+	System.out.println("Your name is "+name);
+	sc.close();
+}
+}
---
+++ InputNameFromUser.java
@@ -0,0 +1,9 @@
+package com.javaclass;
+import jayanthi.CCBO;
+public class InputNameFromUser {
+	public static void main(String[] args) {
+		
+	jayanthi.CCBO o = new CCBO();
+	o.display();
+}
+}
